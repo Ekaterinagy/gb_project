081d984de8285556b34adab7f04ac5073c3b0f98
ekaterinagy <mishinaekaterin@gmail.com>
2023-11-09 15:38:28 +0400

    Seminar 1 work

---
+++ lesson1/main.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# python ../gb_project/lesson1/main.py
+if __name__ == '__main__':
+    print('PyCharm')
+    result = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    out = result.stdout
+    if '20.04.6' in out and 'focal' in out and result.returncode == 0:
+        print('SUCCESS')
+    else:
+        print('FAIL')
